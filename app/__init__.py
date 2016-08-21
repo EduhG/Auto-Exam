1d45e064d1b760720a5e4c1c9c44bc2b1f8334d0
eduhg <edwinnyangena@gmail.com>
2016-08-21 17:49:03 +0300

    Added routes to home.py

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from .views.home import home_blueprint
+from .views.profile import profile_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(home_blueprint)
+app.register_blueprint(profile_blueprint)
+
+app.config.from_pyfile('config.py')
---
+++ app/views/home.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+home_blueprint = Blueprint('home', __name__)
+
+
+@home_blueprint.route('/')
+def index():
+    return render_template('home/index.html')
+
+
+@home_blueprint.route('/home/login')
+def login():
+    return render_template('home/login.html')
+
+
+@home_blueprint.route('/home/signup')
+def signup():
+    return render_template('home/signup.html')
