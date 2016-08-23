38ae1151f8157bcde2f261e70af50609258434d8
eduhg <edwinnyangena@gmail.com>
2016-08-23 10:02:34 +0300

    Added plugins

---
+++ app/static/js/autoExam/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_flat',
+        radioClass: 'iradio_flat'
+    });
+
+
+    $("select2").select2({
+        /*placeholder: "Select a state",
+        allowClear: true*/
+    });
+});
