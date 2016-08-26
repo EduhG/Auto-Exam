de754f2c8bc1aa5094c9a36f9d107c9f65c8becf
eduhg <edwinnyangena@gmail.com>
2016-08-26 22:45:18 +0300

    Added submenus to sidebar

--- app/static/js/autoExam/app.js
+++ app/static/js/autoExam/app.js
@@ -103,8 +103,8 @@ var randomScalingFactor1 = function() {
         labels: ["Maths", "English", "Kiswahili", "Science", "Social Studies"],
         datasets: [{
             label: '',
-            backgroundColor: "rgba(220,220,220,0.5)",
-            data: [80, 65, 70, 80, 76]
+            backgroundColor: randomColor(), //"rgba(220,220,220,0.5)",
+            data: [80, 65, 70, 85, 76]
         }]
 
     };
@@ -138,3 +138,28 @@ var randomScalingFactor1 = function() {
             }
         });
     };
+
+
+$('#sidenav>ul').on('click', '.submenu', function (e) {
+    console.log('hey you');
+    var element = $(this);//.parent('li');
+
+    if (element.hasClass('open')) {
+        element.removeClass('open');
+        element.find('li').removeClass('open');
+        element.find('ul').slideUp();
+    }
+    else {
+        element.addClass('open');
+        element.children('ul').slideDown();
+        element.siblings('li').children('ul').slideUp();
+        element.siblings('li').removeClass('open');
+        element.siblings('li').find('li').removeClass('open');
+        element.siblings('li').find('ul').slideUp();
+    }
+
+    if ($(this).attr('href') == '#') {
+        e.preventDefault();
+    }
+
+});
