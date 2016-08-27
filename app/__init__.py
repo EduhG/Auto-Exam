c1623a329ad5b3644dce314c4ae392539c3d367d
eduhg <edwinnyangena@gmail.com>
2016-08-27 23:17:07 +0300

    Added loging

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+import logging
+import sys
 
 db = SQLAlchemy()
 
@@ -10,6 +12,9 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     db.init_app(app)
+
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
 
     from .views.home import home_blueprint
     app.register_blueprint(home_blueprint)
